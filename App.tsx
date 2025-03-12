4118b73069dbff6d56c7a1675406c4266bf38189
José Eduardo <ti2@coopfam.com.br>
2025-03-12 16:30:44 -0300

    Identação

--- App.tsx
+++ App.tsx
@@ -34,79 +34,79 @@ import ResultCopartScreen from './src/screens/coparticipacao/ResultCopartScreen'
 import TermosScreen from './src/screens/termos/TermosScreen';
 
 export type RootStackParamList = {
-    Loading: undefined;
-    SelectIsolada: undefined;
-    Perfil: undefined;
-    Login: undefined;
-    Home: undefined;
-    Intermediary: undefined;
-    Select: undefined;
-    Contato: undefined;
-    WebView: undefined;
-    EscolhaIsolada: undefined;
-    ResultadoBusca: undefined;
-    ResultadoDetalhes: undefined;
-    SelectCombinada: undefined;
-    RecuperarSenha: undefined;
-    About: undefined;
-    PrimeiroAcesso: undefined;
-    Senha: undefined;
-    PrimeiroAcesso_dois: undefined;
-    Financeiro: undefined;
-    AlterarSenha: undefined;
-    AlterarSenhaDois: undefined;
-    Contrato: undefined;
-    Utilizacoes: undefined;
-    Utilizacoes2: undefined;
-    Carteirinha: undefined;
-    Carteirinha_2: undefined;
-    Confirma: undefined;
-    Confirma_notFound: undefined;
-    Coparticipacao: undefined;
-    ResultCopart: undefined;
-    Termos: undefined;
+  Loading: undefined;
+  SelectIsolada: undefined;
+  Perfil: undefined;
+  Login: undefined;
+  Home: undefined;
+  Intermediary: undefined;
+  Select: undefined;
+  Contato: undefined;
+  WebView: undefined;
+  EscolhaIsolada: undefined;
+  ResultadoBusca: undefined;
+  ResultadoDetalhes: undefined;
+  SelectCombinada: undefined;
+  RecuperarSenha: undefined;
+  About: undefined;
+  PrimeiroAcesso: undefined;
+  Senha: undefined;
+  PrimeiroAcesso_dois: undefined;
+  Financeiro: undefined;
+  AlterarSenha: undefined;
+  AlterarSenhaDois: undefined;
+  Contrato: undefined;
+  Utilizacoes: undefined;
+  Utilizacoes2: undefined;
+  Carteirinha: undefined;
+  Carteirinha_2: undefined;
+  Confirma: undefined;
+  Confirma_notFound: undefined;
+  Coparticipacao: undefined;
+  ResultCopart: undefined;
+  Termos: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const App = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Loading" component={LoadingScreen} />
-                <Stack.Screen name="SelectIsolada" component={SelectIsoladaScreen} />
-                <Stack.Screen name="Perfil" component={PerfilScreen} />
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Intermediary" component={IntermediaryScreen} />
-                <Stack.Screen name="Select" component={SelectScreen} />
-                <Stack.Screen name="Contato" component={ContatoScreens} />
-                <Stack.Screen name="WebView" component={WebViewScreen} />
-                <Stack.Screen name="EscolhaIsolada" component={EscolhaIsoladaScreen} />
-                <Stack.Screen name="ResultadoBusca" component={ResultadoBuscaScreen} />
-                <Stack.Screen name="ResultadoDetalhes" component={ResultadoDetalhesScreen} />
-                <Stack.Screen name="SelectCombinada" component={SelectCombinadaScreen} />
-                <Stack.Screen name="RecuperarSenha" component={RecuperarSenhaScreen} />
-                <Stack.Screen name="About" component={AboutScreen} />
-                <Stack.Screen name="PrimeiroAcesso" component={PrimeiroAcessoScreen} />
-                <Stack.Screen name="Senha" component={SenhaScreen} />
-                <Stack.Screen name="PrimeiroAcesso_dois" component={PrimeiroAcesso_doisScreen} />
-                <Stack.Screen name="Financeiro" component={FinanceiroScreen} />
-                <Stack.Screen name="AlterarSenha" component={AlterarSenhaScreen} />
-                <Stack.Screen name="AlterarSenhaDois" component={AlterarSenha_DoisScreen} />
-                <Stack.Screen name="Contrato" component={ContratoScreen} />
-                <Stack.Screen name="Utilizacoes" component={UtilizacoesScreen} />
-                <Stack.Screen name="Utilizacoes2" component={Utilizacoes2Screen} />
-                <Stack.Screen name="Carteirinha" component={CarteirinhaScreen} />
-                <Stack.Screen name="Carteirinha_2" component={Carteirinha_2Screen} />
-                <Stack.Screen name="Confirma" component={ConfirmaScreen} />
-                <Stack.Screen name="Confirma_notFound" component={Confirma_notFoundScreen} />
-                <Stack.Screen name="Coparticipacao" component={CoparticipacaoScreen} />
-                <Stack.Screen name="ResultCopart" component={ResultCopartScreen} />
-                <Stack.Screen name="Termos" component={TermosScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Loading" component={LoadingScreen} />
+        <Stack.Screen name="SelectIsolada" component={SelectIsoladaScreen} />
+        <Stack.Screen name="Perfil" component={PerfilScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Intermediary" component={IntermediaryScreen} />
+        <Stack.Screen name="Select" component={SelectScreen} />
+        <Stack.Screen name="Contato" component={ContatoScreens} />
+        <Stack.Screen name="WebView" component={WebViewScreen} />
+        <Stack.Screen name="EscolhaIsolada" component={EscolhaIsoladaScreen} />
+        <Stack.Screen name="ResultadoBusca" component={ResultadoBuscaScreen} />
+        <Stack.Screen name="ResultadoDetalhes" component={ResultadoDetalhesScreen} />
+        <Stack.Screen name="SelectCombinada" component={SelectCombinadaScreen} />
+        <Stack.Screen name="RecuperarSenha" component={RecuperarSenhaScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="PrimeiroAcesso" component={PrimeiroAcessoScreen} />
+        <Stack.Screen name="Senha" component={SenhaScreen} />
+        <Stack.Screen name="PrimeiroAcesso_dois" component={PrimeiroAcesso_doisScreen} />
+        <Stack.Screen name="Financeiro" component={FinanceiroScreen} />
+        <Stack.Screen name="AlterarSenha" component={AlterarSenhaScreen} />
+        <Stack.Screen name="AlterarSenhaDois" component={AlterarSenha_DoisScreen} />
+        <Stack.Screen name="Contrato" component={ContratoScreen} />
+        <Stack.Screen name="Utilizacoes" component={UtilizacoesScreen} />
+        <Stack.Screen name="Utilizacoes2" component={Utilizacoes2Screen} />
+        <Stack.Screen name="Carteirinha" component={CarteirinhaScreen} />
+        <Stack.Screen name="Carteirinha_2" component={Carteirinha_2Screen} />
+        <Stack.Screen name="Confirma" component={ConfirmaScreen} />
+        <Stack.Screen name="Confirma_notFound" component={Confirma_notFoundScreen} />
+        <Stack.Screen name="Coparticipacao" component={CoparticipacaoScreen} />
+        <Stack.Screen name="ResultCopart" component={ResultCopartScreen} />
+        <Stack.Screen name="Termos" component={TermosScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
