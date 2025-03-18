389b8d8c684c13977d69da29335796ead09535c5
José Eduardo <ti2@coopfam.com.br>
2025-03-18 16:03:38 -0300

    primeiro acesso

--- src/screens/primeiroAcesso/PrimeiroAcessoScreen.js
+++ src/screens/primeiroAcesso/PrimeiroAcessoScreen.js
@@ -12,12 +12,10 @@ import {
 import {
     Text,
     Container,
-    Form,
-    Item,
     Input,
     Button,
-    Icon,
-    Spinner
+    Spinner,
+    NativeBaseProvider
 } from 'native-base';
 import { TextInputMask } from 'react-native-masked-text';
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -42,14 +40,14 @@ const PrimeiroAcesso = () => {
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        console.log("Primeiro acesso carregado");
+        console.log(isLoading);
     }, []);
 
     const onSubmit = () => {
         if (cpf && dataNascimento && matricula && mae) {
             setIsLoading(true);
             const url = `${Server.API}primeiroAcesso/testeFirst.asp?matricula=${matricula}`;
-            
+
             fetch(url)
                 .then(response => response.json())
                 .then(responseJson => {
@@ -76,7 +74,7 @@ const PrimeiroAcesso = () => {
         if (formattedCpf === data.cpf && dataNascimento === data.nascimento && matricula === data.matricula) {
             let nomeMae = mae.trim().toUpperCase();
             let primeiroNomeMae = data.nome_mae.split(" ")[0].toUpperCase();
-            
+
             if (nomeMae === primeiroNomeMae) {
                 let codigo = Math.random().toString().slice(-6);
                 navigation.navigate(data.email ? 'Confirma' : 'PrimeiroAcesso_dois', {
@@ -98,49 +96,60 @@ const PrimeiroAcesso = () => {
     };
 
     return (
-        <Container>
-            <StatusBar backgroundColor={ColorsScheme.ASENT_COLOR} barStyle="light-content" />
-            <ImageBackground source={require('../../assets/fundoNovo.png')} style={styles.background}>
-                {isLoading ? (
-                    <View style={styles.loadingContainer}>
-                        <Spinner size={85} color={ColorsScheme.MAIN_COLOR} />
-                        <Text style={styles.loadingText}>Carregando...</Text>
-                    </View>
-                ) : (
-                    <ScrollView>
-                        <View style={styles.logoContainer}>
-                            <Image style={styles.logo} source={require('../../assets/Logo_MEDGLO_POS.png')} resizeMode="contain" />
+        <NativeBaseProvider>
+            
+                <StatusBar backgroundColor={ColorsScheme.ASENT_COLOR} barStyle="light-content" />
+                <ImageBackground source={require('../../assets/fundoNovo.png')} style={styles.background}>
+                    {isLoading ? (
+                        <View style={styles.loadingContainer}>
+                            <Spinner size={85} color={ColorsScheme.MAIN_COLOR} />
+                            <Text style={styles.loadingText}>Carregando...</Text>
                         </View>
-                            <Text style={{ fontSize: 22, fontWeight: "bold", textAlign: "center", padding: 10, color: ColorsScheme.ASENT_COLOR }}>
+                    ) : (
+                        <ScrollView style={{ width: "100%" }}>
+                            <View style={styles.logoContainer}>
+                                <Image style={styles.logo} source={require('../../assets/Logo_MEDGLO_POS.png')} resizeMode="contain" />
+                            </View>
+                            <Text style={{ fontSize: 22, fontWeight: "bold", textAlign: "center", padding: 10, color: "#000000" }}>
                                 PRIMEIRO ACESSO
                             </Text>
-                        <View style={styles.formContainer}>
-                            <View style={styles.formItem}>
-                                <Text>Matrícula:</Text>
-                                <Input keyboardType='number-pad' value={matricula} onChangeText={setMatricula} />
+                            <View style={styles.formContainer}>
+                                <View style={styles.formItem}>
+                                    <Text>Matrícula:</Text>
+                                    <Input keyboardType='number-pad' value={matricula} onChangeText={setMatricula} />
+                                </View>
+                                <View style={styles.formItem}>
+                                    <Text>CPF:</Text>
+                                    <TextInputMask 
+                                        type="cpf" 
+                                        value={cpf} 
+                                        onChangeText={setCpf} 
+                                        keyboardType='number-pad' 
+                                        style={styles.input} 
+                                    />
+                                </View>
+                                <View style={styles.formItem}>
+                                    <Text>Data de nascimento:</Text>
+                                    <TextInputMask type="datetime" options={{ format: "DD/MM/YYYY" }} value={dataNascimento} onChangeText={setDataNascimento} style={styles.input} />
+                                </View>
+                                <View style={styles.formItem}>
+                                    <Text>Primeiro nome da mãe:</Text>
+                                    <Input value={mae} onChangeText={setMae} />
+                                </View>
+                                <View style={styles.buttonContainer}>
+                                    <Button style={styles.button} onPress={onSubmit}>
+                                        <Text style={{
+                                            color: "#FFFFFF"
+                                        }}>
+                                            Prosseguir
+                                        </Text>
+                                    </Button>
+                                </View>
                             </View>
-                            <View style={styles.formItem}>
-                                <Text>CPF:</Text>
-                                <TextInputMask type="cpf" value={cpf} onChangeText={setCpf} keyboardType='number-pad' style={styles.input} />
-                            </View>
-                            <View style={styles.formItem}>
-                                <Text>Data de nascimento:</Text>
-                                <TextInputMask type="datetime" options={{ format: "DD/MM/YYYY" }} value={dataNascimento} onChangeText={setDataNascimento} style={styles.input} />
-                            </View>
-                            <View style={styles.formItem}>
-                                <Text>Primeiro nome da mãe:</Text>
-                                <Input value={mae} onChangeText={setMae} />
-                            </View>
-                            <View style={styles.buttonContainer}>
-                                <Button style={styles.button} onPress={onSubmit}>
-                                    <Text>Prosseguir</Text>
-                                </Button>
-                            </View>
-                        </View>
-                    </ScrollView>
-                )}
-            </ImageBackground>
-        </Container>
+                        </ScrollView>
+                    )}
+                </ImageBackground>
+        </NativeBaseProvider>
     );
 };
 
@@ -153,9 +162,11 @@ const styles = StyleSheet.create({
     headerText: { padding: 10, textAlign: 'center', fontWeight: 'bold' },
     formContainer: { padding: 10 },
     formItem: { flexDirection: 'column', alignItems: 'flex-start', paddingTop: 10 },
-    input: { width: '100%', fontSize: 14, color: "#000" },
+    input: { width: '100%', fontSize: 14, color: "#000", borderWidth: 1, 
+        borderRadius: 5,
+        borderColor: "#c2c2c2" },
     buttonContainer: { flexDirection: 'row', justifyContent: 'flex-end' },
-    button: { margin: 10, backgroundColor: ColorsScheme.ASENT_COLOR },
+    button: { margin: 10, backgroundColor: ColorsScheme.ASENT_COLOR, borderRadius: 20 },
     borderRadius: 10
 });
 
